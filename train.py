826c35e7978d54b88cd5cde0a76fbf03450338bd
Pranav <pranavsk08@gmail.com>
2022-11-26 22:45:25 +0530

    initial commit

--- train.py
+++ train.py
@@ -7,7 +7,7 @@ import time
 from copy import deepcopy
 from pathlib import Path
 from threading import Thread
-
+import this
 import numpy as np
 import torch.distributed as dist
 import torch.nn as nn
